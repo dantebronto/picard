22d101be86cf96030b70e664f55356484f84d31e
Clint Ecker <clint@arstechnica.com>
2010-02-15 01:26:49 +0800

    Post data shouldn't be decoded until the data has been split apart. Closes gh-4

--- picard/lib/picard/request_extensions.js
+++ picard/lib/picard/request_extensions.js
@@ -7,10 +7,10 @@ var request_extensions = {
 
   extract_form_params: function(chunk){
     if( chunk == undefined ) { return }
-    var chunks = decodeURIComponent(chunk).split('&')
+    var chunks = chunk.replace(/\+/g, '%20').split('&')
     for(var i in chunks){
       var k_v = chunks[i].split('=')
-      this[k_v[0]] = k_v[1]
+      this[k_v[0]] = decodeURIComponent(k_v[1])
     }
   },
   
