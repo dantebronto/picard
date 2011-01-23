c4ed98fc8a26f350a64638a9791c52500743c3bb
Kellen Presley <klpresley@gmail.com>
2011-01-23 17:00:10 -0500

    chaining example

--- examples/misc/app.js
+++ examples/misc/app.js
@@ -1,14 +1,17 @@
-var Picard = require('../../lib/picard')
+var Picard = require('../../lib/picard').
 
-Picard.post('/with_params', function(params){
+// Many top-level functions return 'this', allowing
+// for chaining of routes without calling Picard.globalize()
+
+post('/with_params', function(params){
   return { text: '<h1>' + params.foo + ' ' + params.baz + '</h1>' }
-})
- 
-Picard.put('/weapon/:id', function(params){
+}).
+
+put('/weapon/:id', function(params){
   return { text: '<p>Phaser with id #' + params.id + ' set to stun</p>' }
-})
- 
-Picard.del('/fire/:number', function(params){  
+}).
+
+del('/fire/:number', function(params){  
   var text = '<p>Borg cube destroyed using ' + params.number + ' photon torpedoes</p>'
   
   if (  Number(params.number) > 12 )
